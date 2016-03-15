b512f892195ae56c8840cc0e2a10c993940f9ac8
JeffChiu <jchiu@KCC.local>
2016-03-15 14:41:07 -0700

    updated didFailLoadWithError

--- SaferiTNG/WebviewViewController.m
+++ SaferiTNG/WebviewViewController.m
@@ -35,6 +35,53 @@
 }
 
 
+//- (void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+//{
+//    [self.activityIndicator stopAnimating];
+//    
+//    UIAlertController *alert =
+//    [UIAlertController alertControllerWithTitle:@"Load failed"
+//                                        message:error.localizedDescription
+//                                 preferredStyle:UIAlertControllerStyleAlert
+//     ];
+//    UIAlertAction *homeButton =
+//    [UIAlertAction actionWithTitle:@"Home"
+//                             style:UIAlertActionStyleDefault
+//                           handler:^(UIAlertAction * _Nonnull action)
+//     {
+//         [self loadWebPage:@"http://www.mobilemakers.co"];
+//     }];
+//    UIAlertAction *cancelButton =
+//    [UIAlertAction actionWithTitle:@"Cancel"
+//                             style:UIAlertActionStyleDefault
+//                           handler:^(UIAlertAction * _Nonnull action) {}
+//     ];
+//    [alert addAction:homeButton];
+//    [alert addAction:cancelButton];
+//    [self presentViewController:alert animated:YES completion:nil];
+//}
+
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(nullable NSError *)error
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"load failed" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *goHomeButton =
+        [UIAlertAction actionWithTitle:@"Go home"
+                                 style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * _Nonnull action)
+    {
+        [self loadWebPage:@"http://www.mobilemakers.co"];
+    }];
+    
+    UIAlertAction *cancelButton = [UIAlertAction actionWithTitle:@"Cancel" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action)
+        {
+            
+        }
+        ];
+    [alertController addAction:goHomeButton];
+    [alertController addAction:cancelButton];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 
 
@@ -50,4 +97,5 @@
     //NSURLRequest *request = [[NSURLRequest requestWithURL:[NSURL URLWithString:webString]];
     [self.webView loadRequest:request];
 }
+    
 @end
